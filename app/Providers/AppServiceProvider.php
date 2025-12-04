d1666c3e3e72da72b586adaf79dc032c166f33e7
tianleonardos <tianleoardos586@gmail.com>
2025-12-04 11:27:13 +0700

    Force Https buat css

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Force HTTPS di production
         if($this->app->environment('production')) {
-            URL::forceScheme('https');   //
+            URL::forceScheme('https');
+        }
     }
 }
